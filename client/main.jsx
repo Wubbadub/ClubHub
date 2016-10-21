9ef1dd0c0bbf8b859acd16e3485f2d23ab5c7cd0
Ben Hawker <ben@hawker.cc>
2016-10-21 14:15:31 -0700

    Set up spectre as styling framework

--- client/main.jsx
+++ client/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {render} from 'react-dom'
 import ClubHub from 'ClubHub'
 
+// Import Styles
+import './styles/main.less'
+
 render (
   <ClubHub />,
   document.getElementById('app')
