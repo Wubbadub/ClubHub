b5b88e7652452517b654896207e175c48bb13436
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2016-10-22 16:02:00 -0700

    Added image loader and updated splash text \nStill needs to be styled

--- client/components/pages/Splash.jsx
+++ client/components/pages/Splash.jsx
@@ -3,14 +3,31 @@ import React, {PropTypes} from 'react'
 // use when state and lifecycle functions are not needed
 const Splash = (props) => {
   // const {} = props
+  const containerStyle = {
+    backgroundImage: 'url(../../img/landing-bg.png)',
+    justifyContent: 'center',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    height: 'calc(100vh - 6vh)',
+    minHeight: '400px',
+    margin: '3vh',
+    display: 'flex',
+    backgroundColor: '999'
+  }
+
+  const landingContentStyle = {
+    color: '#fff'
+  }
 
   return (
-    <div className="container">
+    <div className="container" style={containerStyle} >
       <div className="columns">
         <div className="column col-4"></div>
-        <div className="column col-4">
-        <p>
-          We make it really easy for club leaders to make great looking websites. Not only does it take less than 10 minutes to make a website with us, it's even easier to maintain and transfer ownership to your new club leader. Shoot us an <a href="mailto:contact@hubsite.club" target="_blank">email</a> if you have any questions, or want to learn more about what we do.
+        <div className="column col-4" style={landingContentStyle}>
+        <p >
+          You want a first point of contact to your university club for potential members and sponsors.
+          We offer a simple Q&A interface for university clubs to create beautiful, minimal websites.
+          Shoot us an <a href="mailto:contact@hubsite.club" target="_blank">email</a> if you have any questions, or want to learn more about what we do.
         </p>
         <a className="btn btn-lrg btn-block btn-primary" href="/create">Create a Website</a>
         </div>
--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,13 @@ module.exports = {
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loaders: ['file']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
