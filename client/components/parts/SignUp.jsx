84fc4caf5a8dcc6ce69f331d77722330b0a0b483
Ben Hawker <ben@hawker.cc>
2016-10-24 19:01:55 -0700

    Add basic sign up

---
+++ client/components/parts/SignUp.jsx
@@ -0,0 +1,45 @@
+import React, {PureComponent} from 'react'
+
+// use when state and lifecycle functions are needed
+export default class SignUp extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      valid: false
+    }
+  }
+
+  static propTypes = {
+    active: React.PropTypes.bool,
+    close: React.PropTypes.function
+  }
+
+  static defaultProps = {
+    active: false
+  }
+
+  render() {
+
+    return (
+      <section>
+        <div className={`modal modal-sm ${this.props.active ? ' active' : ''}`}>
+          <div className="modal-overlay"></div>
+          <div className="modal-container">
+            <div className="modal-header">
+              <button className="btn btn-clear float-right" onClick={this.props.close}></button>
+              <h3 className="modal-title">Sign Up for ClubHub</h3>
+            </div>
+            <div className="modal-body">
+              <div className="content">
+                <p>Hello!</p>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button className="btn btn-primary">Create Site</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
