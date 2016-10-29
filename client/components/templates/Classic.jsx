5afe43ae236a6726e613f1b5663b4038b19e36c6
Kelsey Legault <kelsey.legault@gmail.com>
2016-10-29 11:32:12 -0700

    Add reactified hubsite

---
+++ client/components/templates/Classic.jsx
@@ -0,0 +1,15 @@
+import React, {PureComponent} from 'react'
+
+export default class Classic extends PureComponent{
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+    <div className="site theme-classic">
+      Lol
+    </div>
+    )
+  }
+}
--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {Router, Route, browserHistory} from 'react-router'
+
+// Our Pages
 import ClubHub from 'ClubHub'
 import Editor from 'pages/Editor'
-import {Router, Route, browserHistory} from 'react-router'
+import Classic from 'templates/Classic'
 
 // Import Styles
 import './styles/main.less'
@@ -11,5 +14,6 @@ render((
 <Router history={browserHistory}>
   <Route path="/" component={ClubHub}/>
   <Route path="/editor" component={Editor}/>
+  <Route path="/site/classic" component={Classic}/>
 </Router>
 ), document.getElementById('app'))
