a5e4610c9dc121546cc41bdda3295cb5fd9e3e05
Brynn Hawker <brynn@hawker.me>
2016-11-04 12:16:59 -0700

    Add dirty bit

--- client/components/pages/editor/Editor.jsx
+++ client/components/pages/editor/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, {Component, PropTypes} from 'react'
 import classNames from 'classnames'
 
 import Icon from 'parts/Icon'
@@ -12,6 +12,7 @@ export default class Editor extends Component {
     this.state = {
       sectionStates: this.makeSiteSections(),
       showEditorBar: true,
+      dirtyBit: false,
       site: this.props.site
     }
   }
@@ -30,23 +31,23 @@ export default class Editor extends Component {
   }
 
   toggleEditorBar = () => {
-    this.setState({ showEditorBar: !this.state.showEditorBar })
+    this.setState({showEditorBar: !this.state.showEditorBar})
   }
 
   toggleSection = (s) => {
     const sections = this.state.sectionStates
     sections[s] = !sections[s]
-    this.setState({ sectionStates: sections })
+    this.setState({sectionStates: sections})
   }
 
   setData = (section, data) => {
     const s = this.state.site
     s.sections[section] = data
-    this.setState({ site: s })
+    this.setState({site: s, dirtyBit: true})
   }
 
   handleSubmit = () => {
-    fetch(`http://www.hubsite.club/api/site/${this.props.siteId}`, {
+    const res = fetch(`http://www.hubsite.club/api/site/${this.props.siteId}`, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -54,13 +55,14 @@ export default class Editor extends Component {
       },
       body: JSON.stringify(this.state.site)
     })
+    if (res) this.setState({dirtyBit: false})
   }
 
   render() {
     return (
       <div className="editor container">
         <div className="columns">
-          <div className={classNames('editor-bar', 'col-3', { 'active': this.state.showEditorBar })}>
+          <div className={classNames('editor-bar', 'col-3', {'active': this.state.showEditorBar})}>
             <button type="button" className="toggle" onClick={this.toggleEditorBar}><Icon icon="chevron_right" /></button>
             <div className="editor-viewbox">
               <div className="accordion">
@@ -78,7 +80,7 @@ export default class Editor extends Component {
                 })}
               </div>
               <div className="editor-footer">
-                <button type="button" className="btn btn-primary btn-save" onClick={this.handleSubmit}><Icon icon="cloud_upload"/>&nbsp;&nbsp;Save</button>
+                <button type="button" className={classNames('btn', 'btn-primary', 'btn-save', {disabled: !this.state.dirtyBit})} onClick={this.handleSubmit}><Icon icon="cloud_upload"/>&nbsp;&nbsp;Save</button>
               </div>
             </div>
           </div>
--- client/components/pages/site/Site.jsx
+++ client/components/pages/site/Site.jsx
@@ -5,7 +5,6 @@ import * as Themes from './themes'
 export default class Site extends Component{
   constructor(props){
     super(props)
-    console.log(props.site)
   }
 
   static propTypes = {
