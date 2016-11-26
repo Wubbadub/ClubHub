87d850d11a9cb113ee16da808cac15f9debcc01e
Brendon Earl <bearl@uvic.ca>
2016-11-26 11:24:02 -0800

    Fix editor scrolling

--- client/components/pages/editor/Editor.jsx
+++ client/components/pages/editor/Editor.jsx
@@ -16,7 +16,8 @@ export default class Editor extends Component {
       sectionStates: this.makeSiteSections(),
       showEditorBar: true,
       dirtyBit: false,
-      site: this.props.site
+      site: this.props.site,
+      bodyScroll: true
     }
   }
 
@@ -65,16 +66,30 @@ export default class Editor extends Component {
     if (res) this.setState({dirtyBit: false})
   }
 
+  disableBodyScroll = () => {
+    this.setState({bodyScroll: false})
+    document.body.style.overflow = 'hidden'
+  }
+
+  enableBodyScroll = () => {
+    this.setState({bodyScroll: true})
+    document.body.style.overflow = ''
+  }
+
   render() {
     return (
       <div className="editor container">
         <div className="columns">
-          <div className={classNames('editor-bar', 'col-3', {'active': this.state.showEditorBar})}>
+          <div className={classNames('editor-bar', 'col-3', {'active': this.state.showEditorBar})} onMouseEnter={this.disableBodyScroll} onMouseLeave={this.enableBodyScroll}>
             <button type="button" className="toggle" onClick={this.toggleEditorBar}><Icon icon="chevron_right" /></button>
             <div className="editor-header">
               <a href={`http://${Config.host}`} target="_blank">
                 <Brand />
               </a>
+            </div>
+            <div className="editor-footer">
+              <Link className={classNames('btn', 'btn-link')} to={`/`} target="_blank"><Icon icon="eye"/>&nbsp;&nbsp;View Site</Link>
+              <button type="button" className={classNames('btn', 'btn-primary', 'btn-save', {disabled: !this.state.dirtyBit})} onClick={this.handleSubmit}><Icon icon="cloud_upload"/>&nbsp;&nbsp;Save</button>
             </div>
             <div className="editor-viewbox">
               <div className="accordion">
@@ -90,10 +105,6 @@ export default class Editor extends Component {
                       />
                   )
                 })}
-              </div>
-              <div className="editor-footer">
-                <Link className={classNames('btn', 'btn-link')} to={`/`} target="_blank"><Icon icon="eye"/>&nbsp;&nbsp;View Site</Link>
-                <button type="button" className={classNames('btn', 'btn-primary', 'btn-save', {disabled: !this.state.dirtyBit})} onClick={this.handleSubmit}><Icon icon="cloud_upload"/>&nbsp;&nbsp;Save</button>
               </div>
             </div>
           </div>
