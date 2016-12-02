88e6206e78c1b0a9b8f64f92bcfef0d00768fe75
Kelsey Legault <kelsey.legault@gmail.com>
2016-12-01 16:29:01 -0800

    Redesign landing page and make responsive

--- client/components/pages/Splash.jsx
+++ client/components/pages/Splash.jsx
@@ -37,77 +37,163 @@ export default class Splash extends PureComponent {
     return (
       <div className="splash">
         <SignUp active={this.state.signup} close={this.hideSignUp}/>
-        <div className="header">
-          <div className="container-lrg">
-            <div className="col-12 spread">
-              <div>
-                <a><Brand/></a>
-              </div>
-              <div>
-                <Link className="btn btn-link nav-link" to="/editor/create">Log in</Link>
-                <button type="button" className="btn btn-link nav-link" href="#" onClick={this.showSignUp}>Sign up</button>
-                <a className="btn btn-link nav-link" target="_blank" href={`mailto:contact@${Config.host}`}>Contact</a>
-              </div>
-            </div>
-          </div>
-          <div className="container-sml">
-            <div className="col-12 text-center">
-              <h1>Provide the resources your university club needs, all you have to do is answer a few questions.</h1>
-              <h2>
-                You don’t even need to worry about buying a domain or serving your website.
-                We give you a live, beautiful website that's ready to share within the first five minute of joining.
-              </h2>
-              <div className="ctas">
-                <button type="button" className="btn ctas-button" onClick={this.showSignUp}>Start Creating</button>
-              </div>
-            </div>
-          </div>
-          <div className="container-lrg">
-            <div className="col-12">
-              <div className="header-images">
-                <div className="iphone">
-                  <div className="mask">
-                    <img className="mask-img" src={mobileview}/>
+        <div className="template">
+          <div className="header">
+            <header className="header">
+              <div className="container-lrg flex">
+                <div className="col-4">
+                  <a><Brand/></a>
+                  <div>
+                    <Link className="nav-link" to="/editor/create">Log in</Link>
+                    <a className="nav-link" href="#" onClick={this.showSignUp}>Sign up</a>
+                    <a className="nav-link" target="_blank" href={`mailto:contact@${Config.host}`}>Contact</a>
                   </div>
                 </div>
-                <div className="browser">
-                  <div className="mask">
-                    <img className="mask-img" src={browserview}/>
+                <div className="col-8">
+                  <h1 className="header-heading">Take ten minutes to tell us about your universty club.</h1>
+                  <h2 className="header-subheading">In return, we'll generate a beautiful website for you that's ready to share.</h2>
+                  <div className="ctas">
+                    <div className="onecta"><a className="ctas-button" href="#" onClick={this.showSignUp}>Create my club website</a></div>
                   </div>
                 </div>
               </div>
+              <div className="container-lrg">
+                <div className="col-12">
+                  <div className="header-images">
+                    <div className="header-images">
+                      <div className="centerdevices">
+                        <div className="iphoneipad2">
+                          <div className="iphone">
+                            <div className="mask">
+                              <img className="mask-img" src={mobileview}/>
+                            </div>
+                          </div>
+                          <div className="ipad">
+                            <div className="mask">
+                              <img className="mask-img" src={browserview}/>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </header>
+          </div>
+          <section>
+            <ul>
+              <div className="feature5 template">
+                <div className="container-sml text-center">
+                  <h3>It's very simple. Here's all you have to do:</h3>
+                </div>
+                <div className="container-lrg flex">
+                  <div className="col-6 centervertical">
+                    <div className="f5">
+                      <div className="steps">
+                        <div className="steps-img">💬</div>
+                        <h3>Answer some questions</h3>
+                        <p className="header-subheading">What's your club name? What days do you meet? What do you want your website address to be?</p>
+                      </div>
+                      <div className="steps">
+                        <div className="steps-img">✏️</div>
+                        <h3>Customize your content</h3>
+                        <p className="header-subheading">Add more content that's relevant to your club like social media links and important club members</p>
+                      </div>
+                      <div className="steps">
+                        <div className="steps-img">🌎</div>
+                        <h3>Save, it's ready to share!</h3>
+                        <p className="header-subheading">Start sharing your website with members, potential recruits, and sponsors!&nbsp;</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="header-images">
+                      <div className="header-images">
+                        <div className="sidedevices">
+                          <div className="computerwrapper">
+                            <div className="computer">
+                              <div className="mask"><img className="mask-img" src={browserview}/></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </ul>
+          </section>
+          <div className="socialprooftemplate">
+            <div className="template">
+              <div className="container-sml">
+                <div className="flex text-center">
+                  <div className="col-12">
+                    <h3 className=" editable">Join the Clubhub community.&nbsp;</h3></div>
+                  </div>
+                </div>
+                <div className="container-lrg">
+                  <div className="tweets flex">
+                    <div className="col-4">
+                      <div className="sp-tweets">
+                        <div className="flex flexvert">
+                          <div>
+                            <b>The UVic Art Hive</b>
+                            <br/>
+                            <a target="_blank" href="http://webdev.uvic.club">hybrid.uvic.club</a>
+                          </div>
+                        </div>
+                        <div className="sp-tweets-content">
+                          <p>"The best part was choosing the image!"</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-4">
+                      <div className="sp-tweets">
+                        <div className="flex flexvert">
+                          <div>
+                            <b>UVic Web Dev Club</b>
+                            <br/>
+                            <a target="_blank" href="http://webdev.uvic.club">webdev.uvic.club</a>
+                          </div>
+                        </div>
+                        <div className="sp-tweets-content">
+                          <p>"We didn't even have to buy a domain name. It was is revolutionary"</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-4">
+                      <div className="sp-tweets">
+                        <div className="flex flexvert">
+                          <div>
+                            <b>UVic Formula Hybrid</b>
+                            <br/>
+                            <a target="_blank" href="http://webdev.uvic.club">hybrid.uvic.club</a>
+                          </div>
+                        </div>
+                        <div className="sp-tweets-content">
+                          <p>"We now have a beautiful website now to show off to sponsors. Feels good man."</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="container-lrg columns element-container">
-            <div className="col-6 text-center">
-              <img src={seo}/>
-              <h2>
-                We make sure to serve your website right so your club website
-                can gain traffic from organic google searches.
-              </h2>
-            </div>
-            <div className="col-6 text-center">
-              <img src={transfer}/>
-              <h2>
-                Using Clubhub makes it easy for you and your club members to
-                maintain and transfer ownership.
-              </h2>
-            </div>
-          </div>
-          <div className="container-lrg second-cta">
-            <div className="col-12 text-center">
-              <h1>Spend less time sweating the details and more time enjoying your club.</h1>
-              <div className="ctas">
-                <button type="button" className="btn ctas-button" onClick={this.showSignUp}>Get started in minutes</button>
+          <div className="template">
+              <div className="footerblock">
+                  <div className="container-sml flex text-center">
+                      <div className="col-12">
+                          <h3>Stop sweating the details and spend more time enjoying your club.</h3>
+                          <div className="ctas">
+                                <a className="ctas-button" onClick={this.showSignUp}>Create my club website</a>
+                          </div>
+                      </div>
+                  </div>
               </div>
-              <div className="footer">
-                <a><Brand/></a>
-                <a className="footer-link" target="_blank" href={`mailto:contact@${Config.host}`}>Email Us</a>
-              </div>
-            </div>
           </div>
         </div>
-      </div>
     )
   }
 }
